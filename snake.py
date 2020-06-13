67349f725c5441353f4a86f533272f2dee8dde96
Erik van Zijst <erik.van.zijst@gmail.com>
2020-06-12 22:48:34 -0700

    Create snake game
    Little snake game, you know the drill.

---
+++ snake.py
@@ -0,0 +1,70 @@
+from microbit import *
+import random
+
+def paint(board):
+    display.show(Image(":".join(("".join(map(str, row)) for row in board))))
+
+def value(board, x, y):
+    return board[y][x]
+
+def set_off(board, x, y):
+    board[y][x] = 0
+
+def set_on(board, x, y):
+    prev, board[y][x] = board[y][x], 9
+    return prev
+
+def place_food(board):
+    while True:
+        x, y = random.randint(0, 4), random.randint(0, 4)
+        if not (board[y][x]):
+            board[y][x] = 4
+            break
+
+dirs = ((0, -1),    # up
+        (1, 0),     # right
+        (0, 1),     # down
+        (-1, 0))    # left
+
+display.clear()
+display.scroll('snake')
+sleep(1000)
+
+def game():
+    board = [([0] * 5) for _ in range(5)]   # board[y][x]
+    snake = [(2, 2), (2, 3)]
+    tick = 500
+    last = running_time()
+
+    place_food(board)
+    paint(board)
+
+    direction = 0
+    while True:
+        if button_a.was_pressed():
+            direction = (direction - 1) % 4
+        if button_b.was_pressed():
+            direction = (direction + 1) % 4
+    
+        if running_time() - last > tick:
+            # make a move
+            head, tail = snake[0], snake[-1]
+            
+            # new head:
+            head = ((head[0] + dirs[direction][0]) % 5, (head[1] + dirs[direction][1]) % 5)
+            if head in snake:
+                return
+            snake.insert(0, head)
+    
+            if set_on(board, *head):
+                # eat, grow and place new food
+                place_food(board)
+            else:
+                set_off(board, *tail)
+                snake = snake[:-1]
+
+            last = running_time()
+            paint(board)
+        
+game()
+display.scroll('game over')
